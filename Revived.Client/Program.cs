9e16eb89c5bc52c4ec4b9241a59f9833d433db43
Alex Schouls <alex_schouls@live.com>
2021-02-12 18:31:53 +0100

    Client setup

---
+++ Revived.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revived.Client
+{
+    public class Program
+    {
+        private static readonly bool _isAlive = true;
+
+        public static void Main(string[] args)
+        {
+            Task.Run(() => Application.Run(Client.Instance));
+            Loop();
+        }
+
+        private static void Loop()
+        {
+            var backgroundColor = Color.Black;
+            while(_isAlive)
+            {
+                backgroundColor = backgroundColor == Color.Black ? Color.White : Color.Black;
+                Client.Instance.BackColor = backgroundColor;
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
