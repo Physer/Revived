cea325ba6e7d78f3504d61ef27da2f8f4695f7e7
Alex Schouls <alex_schouls@live.com>
2021-02-14 11:53:08 +0100

    Added TCP connectivity between server and client

--- Revived.Client/Program.cs
+++ Revived.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Revived.Client
@@ -6,14 +8,23 @@ namespace Revived.Client
     public class Program
     {
         private static readonly bool _isAlive = true;
+        private static TcpClient _client;
 
         public static void Main(string[] args)
         {
             // Start the client
             Task.Run(() => Application.Run(Client.Instance));
 
+            // Initialize the connection
+            InitializeConnection();
+
             // Load the game loop
             Loop();
+        }
+
+        private static void InitializeConnection()
+        {
+            _client = new TcpClient("localhost", 7529);
         }
 
         private static void Loop()
--- Revived.Server/Program.cs
+++ Revived.Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace Revived.Server
@@ -9,22 +11,29 @@ namespace Revived.Server
     {
         private static readonly bool _isAlive = true;
         private static TcpListener _listener;
+        private static ConcurrentBag<TcpClient> _clients = new ConcurrentBag<TcpClient>();
 
         private const int _portNumber = 7529;
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             WriteLine("Initializing server...");
             InitializeServer();
             WriteLine($"Listening on port {_portNumber}");
             while (_isAlive)
             {
-                var currentKey = ReadKey();
-                if(currentKey.Key == ConsoleKey.Q && currentKey.Modifiers.HasFlag(ConsoleModifiers.Control))
-                {
-                    WriteLine("Shutting down server...");
-                    ShutdownServer();
-                    Environment.Exit(0);
-                }
+                ListenForShutdown();
+                await ListenForConnections();
+
+            }
+        }
+
+        private static async Task ListenForConnections()
+        {
+            var client = await _listener.AcceptTcpClientAsync();
+            if (!(client is null))
+            {
+                WriteLine("Accepted connection");
+                _clients.Add(client);
             }
         }
 
@@ -34,9 +43,15 @@ namespace Revived.Server
             _listener.Start();
         }
 
-        private static void ShutdownServer()
+        private static void ListenForShutdown()
         {
-            _listener.Stop();
+            var currentKey = ReadKey();
+            if (currentKey.Key == ConsoleKey.Q && currentKey.Modifiers.HasFlag(ConsoleModifiers.Control))
+            {
+                WriteLine("Shutting down server...");
+                _listener.Stop();
+                Environment.Exit(0);
+            }            
         }
     }
 }
