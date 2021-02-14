382cc1b293791225b8717abe2a324fc6d395a7da
Alex Schouls <alex_schouls@live.com>
2021-02-14 11:04:51 +0100

    Added TCP Port listener

--- Revived.Server/Program.cs
+++ Revived.Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 using static System.Console;
 
 namespace Revived.Server
@@ -7,24 +8,35 @@ namespace Revived.Server
     public class Program
     {
         private static readonly bool _isAlive = true;
+        private static TcpListener _listener;
+
+        private const int _portNumber = 7529;
         public static void Main(string[] args)
         {
             WriteLine("Initializing server...");
+            InitializeServer();
+            WriteLine($"Listening on port {_portNumber}");
             while (_isAlive)
             {
                 var currentKey = ReadKey();
                 if(currentKey.Key == ConsoleKey.Q && currentKey.Modifiers.HasFlag(ConsoleModifiers.Control))
                 {
                     WriteLine("Shutting down server...");
-                    Shutdown();
+                    ShutdownServer();
                     Environment.Exit(0);
                 }
             }
         }
 
-        private static void Shutdown()
+        private static void InitializeServer()
         {
-            Thread.Sleep(3000);
+            _listener = new TcpListener(IPAddress.Any, _portNumber);
+            _listener.Start();
+        }
+
+        private static void ShutdownServer()
+        {
+            _listener.Stop();
         }
     }
 }
